6e6a6b990f96676033f962780f2bd209f0c45120
Moizes <moizesdutra@gmail.com>
2024-04-05 12:19:37 -0300

    Corigindo fução JS

--- funcao.js
+++ funcao.js
@@ -5,7 +5,7 @@ console.log(resultado)
 return resultado;
 }
 
-function SomarInput()
+/*function somarInput()
 {    
     var numero1 = parseFloat(document.getElementById("num1").value);
     var numero2 = parseFloat(document.getElementById("num2").value);
@@ -21,5 +21,15 @@ function SomarInput()
     document.getElementById("parimpar").innerHTML = resultado + " é um número ímpar.";
 }
     
-}  
+} 
+*/
+
+function somarInput()
+{
+    var n1 = num1.value;
+    var n2 = num2.value;
+    console.log(n1, n2);
+    var resultado = parseInt(n1) + parseInt(n2);
+    console.log(resultado);   
+}
 
